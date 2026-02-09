71a3a47cdd5c7a25535e09c3655893ae478e6f1f
Essau <essau@localhost.localdomain>
2026-02-09 16:55:10 +0300

    Ongeza Service Worker

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Smart Duka: Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Hii inafanya app ifanye kazi bila internet ikishatumiwa mara moja
+});
